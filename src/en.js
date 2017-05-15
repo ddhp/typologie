eb1aa0cb561ebf59b7e2872af9a472a73353e10f
jesse <sevenknee@gmail.com>
2017-05-16 05:45:55 +0800

    update text

--- src/en.js
+++ src/en.js
@@ -2,12 +2,12 @@ export default {
   navCenter: 'n°1 - La boule de pétanque',
   navRight: '',
   aPropos: {
-    title: 'About the project :',
+    title: 'About the project :',
     body: 'Typologie is a bi-annual publication that takes us on a journey to the heart of everyday objects. Its goal is to broaden common perceptions of familiar things by encouraging its audience to see them anew - through the eyes of industrialists, designers and end users who are passionate about them. Measuring 21cm wide by 26 cm high and composed of 60 pages, Each volume of Typologie is devoted to one object and will explore his secrets by documenting their manufacture, detailing their history, and highlighting their enduring relevance. The beauty of each typology will be celebrated in a series of sixty original prints complemented by archival documents and behind-the-scenes photograph as well as a joint interview and an author text. Typologie will be co-published by Collections Typologie and éditions B42 and published in English and French versions for a price of 20 euros per issue.'
   },
 
   lePremierNumero: {
-    title: 'Le premier numéro :',
+    title: 'The first issue :',
     body: [
       'The first issue of Typologie explores the petanque boule. In it we examine all sides of this well-known object by discussing petanque’s affiliation with the universal and ancient tradition of boule games and examining the broad range of techniques used to produce the boule itself. We also explore the role of the individual boule within petanque and its personification of the carefree, recreational side of life that the game represents. Our investigation reveals the rich and layered character behind these seemingly homogenous spheres.',
       'The issue includes an essay on these themes by Philippe Louguet - an architect, urban planner, design theorist and painter. This is complemented by an in-depth joint interview between Bruno Tainturier (industrial designer and co-founder of the Gulliver design agency) Romain Souvignet (the president of Obut) and Philippe Quintais (professional petanque player and world champion). The text is supplemented by photographs from the Obut archives as well as behind-the-scenes shots of the company’s modern plant and boule museum - all taken by the Typologie team.'
@@ -19,8 +19,8 @@ export default {
     body: [
       'The Collections Typologie is an independent publishing studio directed by Raphaël Daufresne and Thélonious Goupil. It was founded in 2016 by seven industrial designers.',
       'Their first project Typologie is an original idea of the team of the Collections Typologie : Guillaume Bloget, Raphaël Daufresne, Adrien Goubet, Thélonious Goupil, Guillaume Jandin, Alexandre d’Orsetti and Yun Li.',
-      'The graphic design of the Typologie n°1 was conceived by Anne-Lise Bachelier and Lisa Sturacci. ',
-      'Typologie is co-published with éditions B42. The books will be distributed by Belles Lettres publishers in France and Belgium. Idea Books will be in charge of Global distribution outside of these countries.',
+      'The graphic design of Typologie 1 was conceived by Anne-Lise Bachelier and Lisa Sturacci.',
+      'Typologie is co-published with Editions B42. The books will be distributed by Belles Lettres publishers in France and Belgium. Idea Books will be in charge of Global distribution outside of these countries.',
       'contact@collectionstypologie.com',
       '@collectionstypologie'
     ]
--- src/fr.js
+++ src/fr.js
@@ -3,26 +3,26 @@ export default {
   navRight: '',
   aPropos: {
     title: 'A propos :',
-    body: 'Typologie est une collection de livres pour voyager à la source des objets qui nous entourent. Son but est d’offrir un regard nouveau sur les objets ordinaires qui permettra aux lecteurs d’apprécier et de redécouvrir le sens de leurs formes. D’un format de 21x26cm et riche de 60 pages, chaque numéro sera consacré à une typologie d’objet dont il documentera l’histoire, explorera les secrets de fabrication et étudiera l’usage. La beauté de ces différent s objets familiers sera mise en valeur à travers soixante reproductions originales complétées par des images d’archives, des photographies d’usines, un texte d’auteur et une interview croisée. Les albums Typologie, co-édités par les Collections Typologie et les Edition B42 dans une version française et une version anglaise, seront distribués au prix de 20 euros.'
+    body: 'Typologie est une collection de livres pour voyager à la source des objets qui nous entourent. Son but est d’offrir un regard nouveau sur les objets ordinaires qui permettra aux lecteurs d’apprécier et de redécouvrir le sens de leurs formes. D’un format de 21x26cm et riche de 60 pages, chaque numéro sera consacré à une typologie d’objet dont il documentera l’histoire, explorera les secrets de fabrication et étudiera l’usage. La beauté de ces différents objets familiers sera mise en valeur à travers soixante reproductions originales complétées par des images d’archives, des photographies d’usines, un texte d’auteur et une interview croisée. Les albums Typologie, co-édités par les Collections Typologie et les Editions B42 dans une version française et une version anglaise, seront distribués au prix de 20 euros.'
   },
 
   lePremierNumero: {
     title: 'Le premier numéro :',
     body: [
-      'Le premier objet présenté dans Typologie est la boule de pétanque. L’étude minutieuse de cet objet, parfaitement banal en apparence, présente un intérêt d’une richesse insoupçonnée : la  liation de la pétanque avec la tradition universelle des jeux de boules, son histoire moderne et populaire, sa production industrielle sophistiquée, la multitude de ses techniques de fabrication que la constance de sa forme permet de mettre en évidence. Mais aussi un contenu sociologique, des règles, un cadre, un glossaire, une gestuelle, et même une philosophie de vie...',
-      'Ces thèmes sont introduits par Philippe Louguet, architecte urbaniste, théoricien du design et peintre. Ce texte est suivi d’une interview  euve qui réunit Bruno Tainturier, designer industriel, co-fondateur de l’agence de design Gulliver, Romain Souvignet, directeur général de l’entreprise Obut et Philippe Quintais, joueur professionnel de pétanque, douze fois champion du monde dans différentes catégories. Ce contenu sera illustrés par des images tirées des archives de l’entreprise Obut, un reportage photographique sur son usine ainsi que par les reproductions d’objets du musée de la Boule Obut, toutes réalisées par l’équipe des Collections Typologie.',
-      'Quand au deuxième numéro, il sera consacré au bouchon en liège !'
+      'Le premier objet présenté dans Typologie est la boule de pétanque. L’étude minutieuse de cet objet, parfaitement banal en apparence, présente un intérêt d’une richesse insoupçonnée : la filiation de la pétanque avec la tradition universelle des jeux de boules, son histoire moderne et populaire, sa production industrielle sophistiquée, la multitude de ses techniques de fabrication que la constance de sa forme permet de mettre en évidence. Mais aussi un contenu sociologique, des règles, un cadre, un glossaire, une gestuelle, et même une philosophie de vie...',
+      'Ces thèmes sont introduits par Philippe Louguet, architecte urbaniste, théoricien du design et peintre. Ce texte est suivi d’une interview croisée qui réunit Bruno Tainturier, designer industriel, co-fondateur de l’agence de design Gulliver, Romain Souvignet, directeur général de l’entreprise Obut et Philippe Quintais, joueur professionnel de pétanque, douze fois champion du monde dans différentes catégories. Ce contenu sera illustré par des images tirées des archives de l’entreprise Obut, un reportage photographique sur son usine ainsi que par des reproductions d’objets du musée de la Boule Obut, toutes réalisées par l’équipe des Collections Typologie.',
+      'Quand au deuxième numéro, il sera consacré au bouchon en liège !'
     ]
   },
 
   contacts: {
     title: 'Contacts :',
     body: [
-      'Contacts  Collections Typologies sont une maison d\'édition indépendante dirigées par Raphaël Daufresne et Thélonious Goupil et fondée en 2016 par sept designers industriels.',
-      'Leur premier projet Typologie est une idée originale de l\'équipe des Collections Typologie formée par Guillaume Bloget, Raphaël Daufresne, Adrien Goubet, Thélonious Goupil, Guillaume Jandin, Alexandre d’Orsetti et Yun Li.',
-      'Le graphisme du premier numéro a été dessiné par Anne-Lise Bachelier et Lisa Sturacci.',
-      'Typologie est co-édité avec les éditions B42.',
-      'La distribution des livres sera assuré par les Belles-Lettres en France en Belgique et en Suisse. Dans les autres pays, la distribution sera pris en charge par Idea Books.',
+      'Les Collections Typologies est une maison d’édition indépendante dirigées par Raphaël Daufresne et Thélonious Goupil et fondée en 2016 par sept designers industriels.',
+      'Leur premier projet Typologie est une idée originale de l\'équipe des Collections Typologie formée par Guillaume Bloget, Raphaël Daufresne, Adrien Goubet, Thélonious Goupil, Guillaume Jandin, Alexandre d’Orsetti et Yun Li.',
+      'La conception graphique du premier numéro a été mis au point par Anne-Lise Bachelier et Lisa Sturacci.',
+      'Typologie est co-édité avec les Editions B42.',
+      'La distribution des livres sera assuré par les Belles-Lettres en France en Belgique et en Suisse. Dans les autres pays, la distribution sera pris en charge par Idea Books.',
       'contact@collectionstypologie.com',
       '@collectionstypologie'
     ]
